532692be67ea1a511a73fdb2275f5d6cadeafcb6
yidaotong <yidaotong@163.com>
2019-04-12 00:00:32 +0800

    add ranking

--- drawlerMovie/crawlerMovie.py
+++ drawlerMovie/crawlerMovie.py
@@ -4,7 +4,7 @@ import requests
 from lxml import etree
 from lxml import html
 from time import time
-import dumper
+#import dumper
 import pprint
 import unicodedata
 import chardet
@@ -15,10 +15,12 @@ class CrawlerMovies:
     def __init__(self, link=b''):
         if link:
             self.rootLink==link
+
         else:
             self.rootLink='https://www.loldytt.tv/'
         self.movieMapQueqe={}
         self.depth=1
+        self.doubanLink = 'https://movie.douban.com/subject_search?search_text='
     def fetchPage(self, url):
         #sock = ssl.wrap_socket(socket.socket())
         #sock=socket.socket()
@@ -31,30 +33,32 @@ class CrawlerMovies:
             #response += chunk
             #chunk=sock.recv(4096)
         #link = self.parseLink(response)
-        print(response)
+        #print(response)
         return response
         #self.movieMapQueqe.add(link)
         
     def parse(self, url):
         response=self.fetchPage(url)
         page=response.content
-        print(page)
+        #print(page)
         htmlText=etree.HTML(page, parser=etree.HTMLParser())
         #print(chardet.detect(htmlText))
-        print(htmlText)
-        pp = pprint.PrettyPrinter(indent=4)
-        print('pprint:')
+        #print(htmlText)
+        #pp = pprint.PrettyPrinter(indent=4)
+        #print('pprint:')
         
-        pp.pprint(htmlText)
-        print('end pprint:')
-        if self.depth == 1:
-            path="//[re:match(text(), '//a']'"
-        else:
-            path="//[re:match(text(), '//li|strong)']"       
-        for link in htmlText.xpath(path, namespaces={"re": "http://exslt.org/regular-expressions"}):
-            print(link.get('href'))
+        #pp.pprint(htmlText)
+        #print('end pprint:')
+
+        path='//a'
+        for link in htmlText.xpath(path):
+            print(link)
             url=link.get('href')
+            print(url)
+            if self.depth == 1:
+                print('depth=2')
             if url:
+                print(link.get('href'))
                 pattenLink=r""+self.rootLink+"(\w+)/$"
                 pattenMovie=r""+self.rootLink+"(\w+)/(\w+)/$"
                 print(pattenLink)
@@ -79,15 +83,22 @@ class CrawlerMovies:
                         self.movieMapQueqe[spec]={}
                     else:
                         if movie not in self.movieMapQueqe[spec]:
-                            if 'title' in link.attrib:
-                                title=link.get('title')
-                                print('title found')
-                                print(title)
-                                self.movieMapQueqe[spec][movie]=title
+                            title=link.get('title')
+                            if title:
+                                self.movieMapQueqe[spec][movie] = title
                             else:
-                                print('not title, get attrib')
-                                print(link.attrib)
-                                self.movieMapQueqe[spec][movie]=b''
+                                fullText=link.xpath('text()')
+                                if fullText:
+                                    text = fullText[0]
+                                if text:
+                                    title=text
+                                    print('title found')
+                                    print(title)
+                                    self.movieMapQueqe[spec][movie]=title
+                                else:
+                                    print('not title, get attrib')
+                                    print(link.attrib)
+                                    self.movieMapQueqe[spec][movie]=b''
                         #elif not self.movieMapQueqe[spec][movie]:
                             #self.movieMapQueqe[spec][movie]=b''
         return b''
@@ -97,6 +108,8 @@ class CrawlerMovies:
         self.depth += 1
     def parseChildPage(self):
         for spec in self.movieMapQueqe.keys():
+            if spec == 'Zuixinriju':
+                print('zuixinriju')
             url=self.rootLink+spec
             print("get child url")
             print(url)
@@ -110,6 +123,19 @@ class CrawlerMovies:
                 url=self.rootLink+spec+'/'+name+'/'
                 movieLinks.append(url)
         return movieLinks
+    def getMovieRankPage(self,movieName):
+        searchLink=self.doubanLink+movieName
+        print(searchLink)
+        response = requests.get(searchLink)
+        return response.content
+    def getMovieRank(self, movieName):
+        page=self.getMovieRankPage(movieName)
+        print(page)
+        htmlText = etree.HTML(page, parser=etree.HTMLParser())
+        path='//div[@class="item-root"]'
+        print(path)
+        for link in htmlText.xpath(path):
+            print(link)
 
 if __name__=='__main__':
     crawlerM=CrawlerMovies()
@@ -117,4 +143,14 @@ if __name__=='__main__':
     crawlerM.parseChildPage()
     print(crawlerM.getMovieQueue())
     print(crawlerM.getAllMovieLinks())
+    keywords = {
+        'search_text': '九品芝麻官',
+    }
+
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 \
+               (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'}
     #print(crawlerM.getMovieQueue())
+    html_post = requests.post(crawlerM.doubanLink, data=keywords)
+    print(html_post)
+    ret=crawlerM.getMovieRank('九品芝麻官')
+    print(ret)
